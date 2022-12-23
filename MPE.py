5bccabbd236e87c6a1dfbaa0a34b38f2d358d685
tj <tj@digitalarsenal.io>
2022-12-22 19:19:12 -0500

    fixed with updated generated files from SDS

--- MPE.py
+++ MPE.py
@@ -94,31 +94,31 @@ class MPE(object):
 def MPEStart(builder): builder.StartObject(9)
 def Start(builder):
     return MPEStart(builder)
-def MPEAddMEAN_MOTION(builder, MEAN_MOTION): builder.Prependfloat64Slot(0, MEAN_MOTION, 0.0)
+def MPEAddMEAN_MOTION(builder, MEAN_MOTION): builder.PrependFloat64Slot(0, MEAN_MOTION, 0.0)
 def AddMEAN_MOTION(builder, MEAN_MOTION):
     return MPEAddMEAN_MOTION(builder, MEAN_MOTION)
-def MPEAddECCENTRICITY(builder, ECCENTRICITY): builder.Prependfloat64Slot(1, ECCENTRICITY, 0.0)
+def MPEAddECCENTRICITY(builder, ECCENTRICITY): builder.PrependFloat64Slot(1, ECCENTRICITY, 0.0)
 def AddECCENTRICITY(builder, ECCENTRICITY):
     return MPEAddECCENTRICITY(builder, ECCENTRICITY)
-def MPEAddINCLINATION(builder, INCLINATION): builder.Prependfloat64Slot(2, INCLINATION, 0.0)
+def MPEAddINCLINATION(builder, INCLINATION): builder.PrependFloat64Slot(2, INCLINATION, 0.0)
 def AddINCLINATION(builder, INCLINATION):
     return MPEAddINCLINATION(builder, INCLINATION)
-def MPEAddRA_OF_ASC_NODE(builder, RA_OF_ASC_NODE): builder.Prependfloat64Slot(3, RA_OF_ASC_NODE, 0.0)
+def MPEAddRA_OF_ASC_NODE(builder, RA_OF_ASC_NODE): builder.PrependFloat64Slot(3, RA_OF_ASC_NODE, 0.0)
 def AddRA_OF_ASC_NODE(builder, RA_OF_ASC_NODE):
     return MPEAddRA_OF_ASC_NODE(builder, RA_OF_ASC_NODE)
-def MPEAddARG_OF_PERICENTER(builder, ARG_OF_PERICENTER): builder.Prependfloat64Slot(4, ARG_OF_PERICENTER, 0.0)
+def MPEAddARG_OF_PERICENTER(builder, ARG_OF_PERICENTER): builder.PrependFloat64Slot(4, ARG_OF_PERICENTER, 0.0)
 def AddARG_OF_PERICENTER(builder, ARG_OF_PERICENTER):
     return MPEAddARG_OF_PERICENTER(builder, ARG_OF_PERICENTER)
-def MPEAddMEAN_ANOMALY(builder, MEAN_ANOMALY): builder.Prependfloat64Slot(5, MEAN_ANOMALY, 0.0)
+def MPEAddMEAN_ANOMALY(builder, MEAN_ANOMALY): builder.PrependFloat64Slot(5, MEAN_ANOMALY, 0.0)
 def AddMEAN_ANOMALY(builder, MEAN_ANOMALY):
     return MPEAddMEAN_ANOMALY(builder, MEAN_ANOMALY)
-def MPEAddNORAD_CAT_ID(builder, NORAD_CAT_ID): builder.Prependuint32Slot(6, NORAD_CAT_ID, 0)
+def MPEAddNORAD_CAT_ID(builder, NORAD_CAT_ID): builder.PrependUint32Slot(6, NORAD_CAT_ID, 0)
 def AddNORAD_CAT_ID(builder, NORAD_CAT_ID):
     return MPEAddNORAD_CAT_ID(builder, NORAD_CAT_ID)
-def MPEAddBSTAR(builder, BSTAR): builder.Prependfloat64Slot(7, BSTAR, 0.0)
+def MPEAddBSTAR(builder, BSTAR): builder.PrependFloat64Slot(7, BSTAR, 0.0)
 def AddBSTAR(builder, BSTAR):
     return MPEAddBSTAR(builder, BSTAR)
-def MPEAddUSER_DEFINED_EPOCH_TIMESTAMP(builder, USER_DEFINED_EPOCH_TIMESTAMP): builder.Prependfloat64Slot(8, USER_DEFINED_EPOCH_TIMESTAMP, 0.0)
+def MPEAddUSER_DEFINED_EPOCH_TIMESTAMP(builder, USER_DEFINED_EPOCH_TIMESTAMP): builder.PrependFloat64Slot(8, USER_DEFINED_EPOCH_TIMESTAMP, 0.0)
 def AddUSER_DEFINED_EPOCH_TIMESTAMP(builder, USER_DEFINED_EPOCH_TIMESTAMP):
     return MPEAddUSER_DEFINED_EPOCH_TIMESTAMP(builder, USER_DEFINED_EPOCH_TIMESTAMP)
 def MPEEnd(builder): return builder.EndObject()
