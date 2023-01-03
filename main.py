8f5a4c87b01aa241e3c35be1a2f38338040bc1dc
tj <tj@digitalarsenal.io>
2023-01-03 14:40:00 -0500

    changed API call

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import requests
 import OMMCOLLECTION
 
-xOMM = requests.get("http://208.87.130.67:3000/spacedata/latest/omm/0x9858effd232b4033e47d90003d41ec34ecaeda94?format=fbs")
+xOMM = requests.get("http://208.87.130.67:3000/spacedatalatest/omm/0x9858effd232b4033e47d90003d41ec34ecaeda94?format=fbs")
 
 yOMMCOLLECTION = OMMCOLLECTION.OMMCOLLECTION.GetRootAsOMMCOLLECTION(xOMM.content)
 
